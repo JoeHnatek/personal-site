8a3f2d9438608aa2f50f471b2856959876a5a1ad
Joseph Hnatek <hnate002@d.umn.edu>
2022-04-16 12:02:11 -0500

    v0.0.1

---
+++ myapp/build/precache-manifest.b0d1b0c758a9929e3c8cae2f054e816e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdb871a07d2f637f6aaa",
+    "url": "/static/css/main.38b968a7.chunk.css"
+  },
+  {
+    "revision": "bdb871a07d2f637f6aaa",
+    "url": "/static/js/main.7a78886f.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  },
+  {
+    "revision": "6ccaca18b0e8792403a8",
+    "url": "/static/js/2.81c6594e.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "cb1686aa088bc36d6feccd9f02c0b811",
+    "url": "/static/media/portrait.cb1686aa.jpg"
+  },
+  {
+    "revision": "f928522fe5a4804e9eb614c06c96718f",
+    "url": "/index.html"
+  }
+];
